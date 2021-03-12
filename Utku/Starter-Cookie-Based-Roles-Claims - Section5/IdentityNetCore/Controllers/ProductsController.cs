7413d97cb5d7a7e97a0f55814402f1b857fcc3bd
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2021-03-12 15:17:36 +0300

    27. Generating a Secure Json Web Token

---
+++ Utku/Starter-Cookie-Based-Roles-Claims - Section5/IdentityNetCore/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityNetCore.Models;
+
+namespace IdentityNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [Route(template:"List")]
+        public List<Product> GetList()
+        {
+            var chair = new Product {Name = "Chair", Price = 100};
+            var desk = new Product {Name = "Desk", Price = 50};
+
+            return new List<Product> {chair, desk};
+        }
+    }
+}
---
+++ Utku/Starter-Cookie-Based-Roles-Claims - Section5/IdentityNetCore/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityNetCore.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+    }
+}
