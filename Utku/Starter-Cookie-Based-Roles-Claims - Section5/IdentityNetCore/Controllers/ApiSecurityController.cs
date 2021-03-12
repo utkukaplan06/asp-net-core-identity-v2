cede18c03e73e2911dcf7d927888debbdc033734
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2021-03-12 16:48:01 +0300

    Method eklendi.

---
+++ Utku/Starter-Cookie-Based-Roles-Claims - Section5/IdentityNetCore/Controllers/ApiSecurityController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityNetCore.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace IdentityNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiSecurityController : ControllerBase
+    {
+        [AllowAnonymous]
+        public async Task<IActionResult> TokenAuth(SigninViewModel model)
+        {
+
+        }
+    }
+}
